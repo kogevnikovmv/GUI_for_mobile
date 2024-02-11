0a864e67bcfc5e9bc6db07c2bb96f79742553ce9
kogevnikovmv <mosaiccat@yandex.ru>
2024-02-11 13:36:46 +0300

    Добавлены виджеты из библиотеки kivymd

--- mobile_ui.py
+++ mobile_ui.py
@@ -1,62 +1,118 @@
 import kivy
 
-from kivy.app import App
+from kivymd.app import MDApp
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
 
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+Builder.load_string('''
+<LoginScreen>:
+    id: login_screen
+
+    MDCard:
+        size_hint_y: None
+        size_hint_x: 0.7
+        pos_hint: {"center_x": 0.5, "center_y": 0.5}
+        elevation: 10
+        padding: 25
+        spacing: 50
+        orientation: 'vertical'
+        height: self.minimum_height
 
 
+        MDLabel:
+            id: welcome_label
+            text: "Hello"
+            font_size: 100
+            halign: 'center'
+            size_hint_y: None
+            height: self.texture_size[1]
+            padding_y: 15
 
-class MainApp(App):
-	def build(self):
-		main_page=BoxLayout(
-			orientation="vertical",
-			padding=50,
-			spacing=50
-		)
-		label = Label(text='Hello, User!', size_hint=(.5, .5), pos_hint={'center_x': .5, 'center_y': .5})
-		main_page.add_widget(label)
+        MDTextField:
+            mode: "round"
+            id: user
+            hint_text: "username"
+            icon_right: "account"
+            size_hint_x: 0.7
+            height: self.minimum_height
 
-		login=TextInput(
-			size_hint=(.5, .1),
-			pos_hint={'center_x': .5, 'center_y': .5}
-		)
-		main_page.add_widget(login)
+            font_size: 50
+            pos_hint: {"center_x": 0.5}
 
-		password=TextInput(
-			size_hint=(.5, .1),
-			pos_hint={'center_x': .5, 'center_y': .5}
-		)
-		main_page.add_widget(password)
+        MDTextField:
+            mode: "round"
+            id: password
+            hint_text: "password"
+            icon_right: "eye-off"
+            size_hint_x: 0.7
+            height: self.minimum_height
 
-		login_button=Button(
-			text='LogIn',
-			size_hint=(.3, .2),
-			pos_hint={'center_x': .5, 'center_y': .5})
-		login_button.bind(on_press=self.login)
-		main_page.add_widget(login_button)
-		return main_page
+            font_size: 50
+            pos_hint: {"center_x": 0.5}
+            password: True
 
-	def login(self, instance):
-		home_page=HomePage()
-		return home_page
+        MDRoundFlatButton:
+            text: "LOG IN"
+            font_size: 50
+            pos_hint: {"center_x": 0.5}
+            on_press: app.logger()
 
-class HomePage(App):
-	def build(self):
-		home_page = BoxLayout(
-			orientation="vertical",
-			padding=50,
-			spacing=50
-		)
-		label = Label(text='It\'s work', size_hint=(.5, .5), pos_hint={'center_x': .5, 'center_y': .5})
-		home_page.add_widget(label)
+        MDRoundFlatButton:
+            text: "CLEAR"
+            font_size: 50
+            pos_hint: {"center_x": 0.5} 
+            on_press: app.clear()           
 
-		return home_page
+        Widget:
+            size_hint_y: None
+            height: 10''')
 
+
+class LoginScreen(Screen):
+    pass
+
+
+class HomeScreen(Screen):
+    def __init__(self, **kwargs):
+        super(HomeScreen, self).__init__(**kwargs)
+        layout = BoxLayout(orientation='vertical')
+
+        logged = Label(text='logged')
+        button_settings = Button(text='Go to settings', on_press=self.go_to_settings)
+
+        layout.add_widget(logged)
+        layout.add_widget(button_settings)
+        self.add_widget(layout)
+
+    def go_to_settings(self, *args):
+        # self.manager.current='settings'
+        print('пока такого экрана нет')
+
+
+class SamuraiPath(MDApp):
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = 'BlueGray'
+        sm = ScreenManager()
+        sm.add_widget(LoginScreen(name='login'))
+        sm.add_widget(HomeScreen(name='home'))
+        return sm
+
+    def logger(self):
+        # self.root.ids.screenmanager.ids.welcome_label.text = f'Sup {self.root.ids.user.text}!'
+        a = self.root.ids.screenmanager.ids.welcome_label.text
+        print(a)
+
+    def clear(self):
+        self.root.ids.welcome_label.text = "Work"
+        self.root.ids.user.text = ""
+        self.root.ids.password.text = ""
 
 
 if __name__ == '__main__':
-	app = MainApp()
-	app.run()
+    SamuraiPath().run()
